49a741e3d73622cf828a2e2d03e26133ca1f4f9a
Sergey <79767637+OddinYa@users.noreply.github.com>
2023-06-06 13:20:58 +0300

    lab#1

---
+++ Л1№20/CalcTwoFormulas/CalcTwoFormulas/CalcTwoFormulas.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+
+float calcFirstForm(int n, int m) {
+
+    float dividend = (m - 1) * sqrt(m) - (n - 1) * sqrt(n);
+    float divider = sqrt(pow(m, 3) * n) + n * m + pow(m, 2) - m;
+
+    return dividend / divider;
+}
+
+float calcSecondForm(int n, int m) {
+
+    float dividend = (sqrt(m) - sqrt(n));
+    float divider = m;
+
+    return dividend / divider;
+}
+
+int main() {
+
+    int n;
+    int m;
+
+    cin >> n >> m;
+
+    printf("First formula: %f \n", (calcFirstForm(n, m)));
+    printf("Second formula:  %f", (calcSecondForm(n, m)));
+
+    return 0;
+}
+
+
+
